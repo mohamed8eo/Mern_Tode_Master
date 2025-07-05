7617e76f8d71b9e0a7ce769abd3c83ccdbb248ff
mohamed <mohamed>
2025-07-05 20:26:19 +0300

    running Fronted on backend URL

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,6 +4,11 @@ import cors from 'cors';
 import { clerkMiddleware } from '@clerk/express'
 import connectDB from './db/todoConnectDB.js';
 import todo from './routes/todo.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 dotenv.config();
@@ -11,16 +16,21 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(express.json());
+if(process.env.NODE_ENV !== 'production') {
+  app.use(cors());
+}
 
 // Routes
 app.use('/api/todos', todo)
 
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the Todo API');
-});
-
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../../Client/dist')));
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../Client/dist/index.html'));
+  });
+}
 
 // Connect to the database and start the server
 connectDB().then(() => {
